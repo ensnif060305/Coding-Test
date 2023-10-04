8fc52a03da2b85ec7d50cb9dcb32a7f739ab25a2
Ensnif <101508194+ensnif060305@users.noreply.github.com>
2023-10-04 17:09:33 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,10 @@
+function solution(d, budget) {
+    let result = 0
+    d.sort((a,b)=> a - b).forEach((e)=>{
+        if(budget >= e){
+            budget -= e
+            result++
+        }else return result
+    })
+    return result
+}
