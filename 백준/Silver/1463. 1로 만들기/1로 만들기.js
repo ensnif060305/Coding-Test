fa69567bfd1355b59ec7ba4c6d97906378f1cae9
Ensnif <101508194+ensnif060305@users.noreply.github.com>
2023-11-08 19:27:28 +0900

    [Silver III] Title: 1로 만들기, Time: 208 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,22 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  const n = Number(line);
+  const dp = new Array(n + 1).fill(0);
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + 1;
+    
+    if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+  }
+
+  console.log(dp[n]);
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
